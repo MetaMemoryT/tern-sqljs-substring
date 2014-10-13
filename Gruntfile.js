5181546c05be6e2a44c075b1c1f1a4a8032b115a
Sean Usick <sean258@gmail.com>
2014-10-13 13:14:01 -0500

    fix crlf in result

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     exec: {
-      create_js: 'sh node_modules/coffee-utils/coffee2js sql.js/coffee/api.coffee && mv sql.js/coffee/api.js .',
+      create_js: 'sh node_modules/coffee-utils/coffee2js --force-lf-line-endings sql.js/coffee/api.coffee && mv sql.js/coffee/api.js .',
       extract_doc: 'node node_modules/tern/bin/condense --disable-doc-comment --name "sqljs-substring" --plugin node_modules/tern-closure/closure.js --def helpers.json api.js > sqljs-substring.json'
     }
   });
